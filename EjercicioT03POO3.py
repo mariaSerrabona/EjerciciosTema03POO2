614b090a435ade03fc7b2b1c503e66b012b276fb
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2021-10-15 16:48:23 +0200

    solucionando errores

--- EjercicioT03POO3.py
+++ EjercicioT03POO3.py
@@ -1,24 +1,24 @@
 #Declaracion de Clases
 class Figura:
     def dibujar(self):
-        print "Dibujar Figura";
- 
+        print ("Dibujar Figura")
+
     def pintar(self):
-        print "Pintando Figura";
- 
+        print ("Pintando Figura")
+
 class Cuadrado(Figura): #Heredando de la clase Figura
     def dibujar(self):   #Redefiniendo Metodo
-        print "Dibujar Cuadrado";
- 
+        print ("Dibujar Cuadrado")
+
 class Triangulo(Figura): #Heredando de la clase Figura
     def dibujar(self):   #Redefiniendo Metodo
-        print "Dibujar Triangulo";
- 
- 
+        print ("Dibujar Triangulo")
+
+
 #Programa Principal
-c = Cuadrado();
-c.dibujar();
-c.pintar();
-t = Triangulo();
-t.dibujar();
-t.pintar();
+c = Cuadrado()
+c.dibujar()
+c.pintar()
+t = Triangulo()
+t.dibujar()
+t.pintar()
