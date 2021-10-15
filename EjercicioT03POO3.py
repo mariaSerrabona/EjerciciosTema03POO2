7894ec0792e1cd12ebe4676ad5e63e21e494dcf7
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2021-10-15 16:43:26 +0200

    iniciando el ejericcio 3 de POO

---
+++ EjercicioT03POO3.py
@@ -0,0 +1,24 @@
+#Declaracion de Clases
+class Figura:
+    def dibujar(self):
+        print "Dibujar Figura";
+ 
+    def pintar(self):
+        print "Pintando Figura";
+ 
+class Cuadrado(Figura): #Heredando de la clase Figura
+    def dibujar(self):   #Redefiniendo Metodo
+        print "Dibujar Cuadrado";
+ 
+class Triangulo(Figura): #Heredando de la clase Figura
+    def dibujar(self):   #Redefiniendo Metodo
+        print "Dibujar Triangulo";
+ 
+ 
+#Programa Principal
+c = Cuadrado();
+c.dibujar();
+c.pintar();
+t = Triangulo();
+t.dibujar();
+t.pintar();
